20a665165f62f74f285f65c3e3b1d385a714d468
FontaMatte <matteo.fontanel@gmail.com>
2022-12-24 12:40:40 +0100

    Bonus1 completed

---
+++ Bonus1/js/script.js
@@ -0,0 +1,84 @@
+// creo array con le immagini
+const images = [
+    'img/01.webp',
+    'img/02.webp',
+    'img/03.webp',
+    'img/04.webp',
+    'img/05.webp'
+];
+
+const carouselElement = document.querySelector('.carousel');
+
+// uso un ciclo for per inserire tutte le immagini in modo dinamico nell' HTML
+for ( let i = 0 ; i < images.length ; i++) {
+    console.log(images[i]);
+
+carouselElement.innerHTML += `<div class="slide">
+                                <img src="${images[i]}">
+                            </div>`;
+}
+
+// importo le slide
+const allSlides = document.querySelectorAll('.slide');
+
+// stampo la classe "current" sulla prima slide per rendere l'immagine visibile
+let currentSlide = 0;
+
+allSlides[currentSlide].classList.add('current');
+
+const previousImage = document.querySelector('.previous');
+const nextImage = document.querySelector('.next');
+
+// al click passo all'immagine sucessiva
+nextImage.addEventListener('click',
+    function() {
+        console.log('cliccato next');
+
+        if ( currentSlide < (images.length - 1)) {
+
+            allSlides[currentSlide].classList.remove('current'); //rimuovo classe "current" alla vecchia slide
+            currentSlide += 1; //ad ogni click incemento l'indice di 1 per cambiare immagine
+
+            allSlides[currentSlide].classList.add('current');  //aagiungo la classe "current" alla nuova slide
+
+            previousImage.classList.remove('hidden');    //rimuovo la classe hidden per rendere visibile la freecia previous  
+
+        } 
+
+        else if (currentSlide == images.length - 1) {
+
+            allSlides[currentSlide].classList.remove('current');
+            currentSlide = 0; 
+            allSlides[currentSlide].classList.add('current');
+
+            
+        }
+    }
+    
+);
+
+// al click passo all'immagine precedente
+previousImage.addEventListener('click',
+    function() {
+        console.log('cliccato previous');
+
+        if ( currentSlide > 0 ) {   
+
+            allSlides[currentSlide].classList.remove('current');
+            currentSlide -= 1;
+
+            allSlides[currentSlide].classList.add('current');
+
+            nextImage.classList.remove('hidden');
+        }
+
+        else if ( currentSlide == 0 ) {
+
+            allSlides[currentSlide].classList.remove('current'); //rimuovo classe "current" alla vecchia slide
+            currentSlide = images.length - 1; //ad ogni click incemento l'indice di 1 per cambiare immagine
+
+            allSlides[currentSlide].classList.add('current');  //aagiungo la classe "current" alla nuova slide        
+       }
+    }
+    
+);
